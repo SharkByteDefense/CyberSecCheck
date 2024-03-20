1ba4322e402cb1aba93e11b0b5a29b240536fd51
SharkByteDefense <125213314+SharkByteDefense@users.noreply.github.com>
2024-03-19 18:44:21 -0700

    up
    up

--- src/store/checklist-context.ts
+++
@@ -1,8 +0,0 @@
-import { type Signal } from '@builder.io/qwik';
-import { createContextId } from '@builder.io/qwik';
-
-import type { Sections } from '../types/PSC';
-
-export const ChecklistContext = createContextId<Signal<Sections>>(
-  'psc.ChecklistContext'
-);
